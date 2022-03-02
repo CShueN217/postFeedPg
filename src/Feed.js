f8a5fe0107fc70d2dc7fe0aac395a404bc954ba7
CShuenLee <40314782+CShuen@users.noreply.github.com>
2022-03-02 22:37:32 +0800

    add tag

--- src/Feed.js
+++ src/Feed.js
@@ -23,37 +23,49 @@ function Feed() {
     return (
         <div className="feed-container">
             {
-                feedList && feedList.length &&
-                feedList.map((feed, index) => {
-                    return (
-                        <div key={index} className="feed-inner-container mg-tp-btm-2rem dark">
-                            <div className="user-box">
-                                <img className="user-icon" src={feed.author.image} alt='user-icon' />
-                                <div className="username">
-                                    {feed.author.username}
-                                    <div>
+                feedList && feedList.length ?
+                    feedList.map((feed, index) => {
+                        return (
+                            <div key={index} className="feed-inner-container mg-tp-btm-2rem dark">
+                                <div className="user-box">
+                                    <img className="user-icon" src={feed.author.image} alt='user-icon' />
+                                    <div className="username-box">
+                                        <span>{feed.author.username}</span>
+                                        <div className="tag-container">
+                                            {
+                                                feed.tagList && feed.tagList.length > 0 &&
+                                                feed.tagList.map((tag, index) => {
+                                                    return (
+                                                        <div className="tag" style={{ border: `2px solid #${index * 2 + feed.tagList.length}666ff` }}>
+                                                            {tag}
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="feed-content-container">
-                                <div className="feedpage-title">
-                                    {feed.title}
+                                <div className="feed-content-container">
+                                    <div className="feedpage-title">
+                                        {feed.title}
+                                    </div>
+                                    <p className="description">{capitalizeFirstLetter(feed.description)}</p>
+                                    <p className="feed-body">{feed.body}</p>
                                 </div>
-                                <p className="description">{capitalizeFirstLetter(feed.description)}</p>
-                                <p className="feed-body">{feed.body}</p>
+                                <div className="post-detail-container">
+                                    {
+                                        feed.favorited ?
+                                            <img className="favorite-icon" src="./heart.png" />
+                                            :
+                                            <img className="favorite-icon" src="./love.png" />
+                                    }
+                                    <div className="favoritesCount">{feed.favoritesCount}</div>
+                                </div>
                             </div>
-                            <div className="post-detail-container">
-                                {
-                                    feed.favorited ?
-                                        <img className="favorite-icon" src="./heart.png" />
-                                        :
-                                        <img className="favorite-icon" src="./love.png" />
-                                }
-                                <div className="favoritesCount">{feed.favoritesCount}</div>
-                            </div>
-                        </div>
-                    )
-                })
+                        )
+                    })
+                    :
+                    null
             }
 
         </div>
