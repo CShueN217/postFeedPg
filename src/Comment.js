d807283f6db5a7e1913b8eded924e7adff0b0f13
CShuenLee <40314782+CShuen@users.noreply.github.com>
2022-03-06 18:55:28 +0800

    delete comment

--- src/Comment.js
+++ src/Comment.js
@@ -6,7 +6,7 @@ import { CommentContext } from './FeedDetail'
 export default function Comment(props) {
     const [feedText, setfeedText] = useState('');
     const { commentList, slug, addComment, setCommentList } = useContext(CommentContext)
-
+    const userName = JSON.parse(sessionStorage.getItem('user')).username;
 
     const postComment = () => {
         if (feedText === '') {
@@ -38,24 +38,47 @@ export default function Comment(props) {
 
     const deleteComment = (id) => {
         console.log(id)
-        axios.delete(`https://api.realworld.io/api/articles/${slug}/comments/${id}`)
+        axios.delete(`https://api.realworld.io/api/articles/${slug}/comments/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`,
+                'Content-type': 'application/json',
+                'Accept': 'application/json',
+                'Method': "DELETE"
+            }
+        })
             .then(res => {
-                console.log('successfully delete comment');
+                console.log('successfully delete comment', res);
+                filterComment(id)
             })
             .catch((error) => {
                 console.log(error)
             })
     }
+
+    const filterComment = (id) => {
+        var list = commentList;
+        var filtered = list.filter(function (value, index, arr) {
+            return value.id != id;
+        });
+        setCommentList(filtered)
+        console.log('filter', filtered)
+    }
+
     return (
         <div className="" >
             {
                 commentList && commentList.length &&
                 commentList.map((comment, index) => {
                     return (
-                        <div className="comment-box" key={index} onClick={() => deleteComment(comment.id)}>
+                        <div className="comment-box" key={index} >
+
                             <div className="comment-user">
                                 <img className="user" src={comment.author.image} alt="user" />
                                 <div className="username-comment">{comment.author.username}</div>
+                                {
+                                    comment.author.username === userName &&
+                                    <div className="delete-icon" onClick={() => deleteComment(comment.id)} />
+                                }
                             </div>
                             <div key={index} className="comment">
                                 {comment.body}
